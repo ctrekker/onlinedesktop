e854c0e28ade39bb11ba6dd30564a145c68046a2
ctrekker <ctrekker4@gmail.com>
2016-10-17 15:54:10 -0600

    Revert "Core Files 0.1.0"
    This reverts commit 83d842bc24ef5cb29a0e65bfeaebf11234673af6.

--- core/lib/security/database.php
+++
@@ -1,6 +0,0 @@
-<?php   
-    $server = new mysqli("localhost", "root", "", "onlinedesktop");
-    if($server->connect_error) {
-        die("Connection failed: ".$server->connect_error);
-    }
-?>

4dbde254486715bff11807326ea1b7e4e70a1911
ctrekker <ctrekker4@gmail.com>
2016-10-21 17:17:51 -0600

    Memory Leak Fixed!
    The memory leak in the code has been fixed, however, there is a slight
    problem with window changes and the Array.prototype.remove() function.

--- core/os/default/apps/test.js
+++ core/os/default/apps/test.js
@@ -1,11 +1,11 @@
 function main() {
     var window=new System.Window(0, 0, 500, 500, "Shapes Test");
-    /*var g=window.graphics;
-    g.add(new Shape(ShapeType.RECTANGLE, 0, 0, 50, 50, "#000"));
-    g.add(new Shape(ShapeType.ELLIPSE, 55, 0, 50, 50, "#000"));   
+    var g=window.graphics;
+    g.add(new Shape(ShapeType.RECTANGLE, 0, 0, 50, 50, "#F00"));
+    g.add(new Shape(ShapeType.ELLIPSE, 55, 0, 50, 50, "#F00"));   
     g.add(new Line(0, 55, 50, 105, "#000")); 
     g.add(new Shape(ShapeType.TEXT, 55, 55, "Hello, World!", {font:"14px Arial"}, "#000"));  
-    g.addComponent(new Button("OK", 100, 100)); */
+    g.addComponent(new Button("OK", 100, 100));
     /*
     THIS CODE HAS A MEMORY LEAK IN IT!!!
     window.show() has been confirmed as the isolated function call causing the initial memory leaking.

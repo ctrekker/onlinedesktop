1c70ad365496b5397ed58c81311fe097a7a84a98
ctrekker <ctrekker4@gmail.com>
2016-10-26 17:59:28 -0600

    Commented base accesslogs.php

--- core/lib/security/accesslogs.php
+++ core/lib/security/accesslogs.php
@@ -1,38 +1,40 @@
 <?php
-    require_once $_SERVER["DOCUMENT_ROOT"].'/api/PDF/autoload.inc.php';
-    
-    use Dompdf\Dompdf;
-    $pdf=new DomPdf();
-    $pdf->loadHtml('
-    <style>
-        tr {
-            border: 2px solid black;
-        }
-        td {
-            border: 2px solid black;
-        }
-    </style>
-    <table>
-        <tr>
-            <td>Box 1</td>
-            <td>Box 2</td>
-        </tr>
-        <tr>
-            <td>Box 3</td>
-            <td>Box 4</td>
-        </tr>
-    </table>
-    ');
+require_once $_SERVER["DOCUMENT_ROOT"].'/api/PDF/autoload.inc.php';
 
-    $pdf->setPaper("A4", "portrait");
+use Dompdf\Dompdf;
+$pdf=new DomPdf();
+$pdf->loadHtml('
+<style>
+    tr {
+        border: 2px solid black;
+    }
+    td {
+        border: 2px solid black;
+    }
+</style>
+<table>
+    <tr>
+        <td>Box 1</td>
+        <td>Box 2</td>
+    </tr>
+    <tr>
+        <td>Box 3</td>
+        <td>Box 4</td>
+    </tr>
+</table>
+');
 
-    $pdf->render();
-    $myfile = fopen("dump.pdf", "w") or die("Unable to open file!");
-    fwrite($myfile, "");
-    fclose($myfile);
-    file_put_contents("dump.pdf", $pdf->output());
+//Set paper to portrait
+$pdf->setPaper("A4", "portrait");
 
-    header("Content-type: application/pdf");
+//Generate file dump.pdf, containing MySQL datalogs dump.
+$pdf->render();
+$myfile = fopen("dump.pdf", "w") or die("Unable to open file!");
+fwrite($myfile, "");
+fclose($myfile);
+file_put_contents("dump.pdf", $pdf->output());
 
-    echo file_get_contents("dump.pdf");
+header("Content-type: application/pdf");
+
+echo file_get_contents("dump.pdf");
 ?>

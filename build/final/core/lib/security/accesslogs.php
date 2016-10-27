e5bb2352c6f0c81c76bb726c716ec4a92590fe03
ctrekker <ctrekker4@gmail.com>
2016-10-27 13:42:10 -0600

    Recompiled, and Made Accesslogs CSV Supportive
    With CSV support, graphs and charts are now easier to generate or create
    using external software such as Excel

--- build/final/core/lib/security/accesslogs.php
+++ build/final/core/lib/security/accesslogs.php
@@ -5,6 +5,8 @@ require 'admins.php';
 session_start();
 use Dompdf\Dompdf;
 
+$content="pdf";
+if(isset($_GET["content"])) $content=$_GET["content"];
 $valid=false;
 //Check admins
 if(isset($_SESSION["id"])) {
@@ -15,84 +17,89 @@ if(isset($_SESSION["id"])) {
     }
 }
 if($valid) {
-    require_once $_SERVER["DOCUMENT_ROOT"].'/api/PDF/autoload.inc.php';
+    if($content="pdf") {
+        require_once $_SERVER["DOCUMENT_ROOT"].'/api/PDF/autoload.inc.php';
 
-    $pdf=new DomPdf();
+        $pdf=new DomPdf();
 
-    $html='
-    <style>
-        table {
-            color: #333;
-            font-family: Helvetica, Arial, sans-serif;
-            font-size: 8pt;
-            width: 640px;
-            border-collapse:
-            collapse; border-spacing: 0;
-        }
-        th {
-            font-size: 10pt;
-        }
-        td, th {
-            border: 1px solid black; /* No more visible border */
-            padding: 0px;
-            margin: 0px;
-        }
+        $html='
+        <style>
+            table {
+                color: #333;
+                font-family: Helvetica, Arial, sans-serif;
+                font-size: 8pt;
+                width: 640px;
+                border-collapse:
+                collapse; border-spacing: 0;
+            }
+            th {
+                font-size: 10pt;
+            }
+            td, th {
+                border: 1px solid black; /* No more visible border */
+                padding: 0px;
+                margin: 0px;
+            }
 
-        th {
-            background: #DFDFDF; /* Darken header a bit */
-            font-weight: bold;
-        }
+            th {
+                background: #DFDFDF; /* Darken header a bit */
+                font-weight: bold;
+            }
 
-        td {
-            background: #FAFAFA;
-            text-align: left;
-            padding-left: 3px;
-        }
-    </style>
-    <table>
-    ';
-    $sql="SELECT * FROM `storage_attempts` ORDER BY `id`";
-    if($result=$server->query($sql)) {
-        $first=true;
-        $count=0;
-        while($row=$result->fetch_assoc()) {
-            if($first) {
+            td {
+                background: #FAFAFA;
+                text-align: left;
+                padding-left: 3px;
+            }
+        </style>
+        <table>
+        ';
+        $sql="SELECT * FROM `storage_attempts` ORDER BY `id`";
+        if($result=$server->query($sql)) {
+            $first=true;
+            $count=0;
+            while($row=$result->fetch_assoc()) {
+                if($first) {
+                    $html.="<tr>";
+                    foreach($row as $key=>$val) {
+                        $html.="<th>".$key."</th>";
+                    }
+                    $html.="</tr>";
+                }
                 $html.="<tr>";
                 foreach($row as $key=>$val) {
-                    $html.="<th>".$key."</th>";
+                    $html.="<td>".$val."</td>";
                 }
                 $html.="</tr>";
-            }
-            $html.="<tr>";
-            foreach($row as $key=>$val) {
-                $html.="<td>".$val."</td>";
-            }
-            $html.="</tr>";
 
-            $first=false;
-            $count++;
+                $first=false;
+                $count++;
+            }
         }
+        else {
+            $html.="<tr><th>Error!</th></tr>";
+        }
+
+        $pdf->loadHtml($html);
+
+        //Set paper to portrait
+        $pdf->setPaper("A4", "portrait");
+
+        //Generate file dump.pdf, containing MySQL datalogs dump.
+        $pdf->render();
+        $myfile = fopen("dump.pdf", "w") or die("Unable to open file!");
+        fwrite($myfile, "");
+        fclose($myfile);
+        file_put_contents("dump.pdf", $pdf->output());
+
+        header("Content-type: application/pdf");
+
+        echo file_get_contents("dump.pdf");
+        unlink("dump.pdf");
     }
-    else {
-        $html.="<tr><th>Error!</th></tr>";
+    else if($content="csv") {
+
     }
-
-    $pdf->loadHtml($html);
-
-    //Set paper to portrait
-    $pdf->setPaper("A4", "portrait");
-
-    //Generate file dump.pdf, containing MySQL datalogs dump.
-    $pdf->render();
-    $myfile = fopen("dump.pdf", "w") or die("Unable to open file!");
-    fwrite($myfile, "");
-    fclose($myfile);
-    file_put_contents("dump.pdf", $pdf->output());
-
-    header("Content-type: application/pdf");
-
-    echo file_get_contents("dump.pdf");
-    unlink("dump.pdf");
 }
 else {
     echo "Access Denied!";
